7a8a080f61699b4921098a7230c5d28378c6b4ff
Michal Sakwa <saka1988@gmail.com>
2019-11-30 00:26:30 +0100

    Pass tenant id to register command handler.

--- IdentityIssuer.WebAPI/Controllers/AuthController.cs
+++ IdentityIssuer.WebAPI/Controllers/AuthController.cs
@@ -10,6 +10,8 @@ namespace IdentityIssuer.WebAPI.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterUserWithCredentialsCommand request)
         {
+            var tenant = await ContextDataProvider.GetTenant(HttpContext);
+            request.TenantId = tenant.TenantId;
             var result = await Mediator.Send(request);
 
             return GetRequestResult(result);
