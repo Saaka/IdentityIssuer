8cc572a523ab404931be1321ada93a170fd440a0
Michal Sakwa <saka1988@gmail.com>
2019-12-13 00:17:34 +0100

    Added custom exception handling for CommandValidationException.

--- IdentityIssuer.WebAPI/Pipeline/CustomExceptionFilterAttribute.cs
+++ IdentityIssuer.WebAPI/Pipeline/CustomExceptionFilterAttribute.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using IdentityIssuer.Common.Exceptions;
 using Microsoft.AspNetCore.Hosting;
 
 namespace IdentityIssuer.WebAPI.Pipeline
@@ -19,7 +22,10 @@ namespace IdentityIssuer.WebAPI.Pipeline
         public override void OnException(ExceptionContext context)
         {
             var exception = context.Exception;
-            HandleApplicationExceptions(context, exception);
+            if (exception is CommandValidationException validationException)
+                HandleValidationException(validationException, context);
+            else
+                HandleApplicationExceptions(context, exception);
         }
 
         private void HandleApplicationExceptions(ExceptionContext context, Exception exception)
@@ -46,6 +52,23 @@ namespace IdentityIssuer.WebAPI.Pipeline
                 Error = context.Exception.Message,
                 ErrorDetails = env.IsDevelopment() ? context.Exception.StackTrace : string.Empty
             });
+        }
+
+        private void HandleValidationException(CommandValidationException validationException, ExceptionContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+            context.Result = new JsonResult(new
+            {
+                Error = validationException.Message,
+                Errors = validationException.Errors.Select(ToCamelCase).ToList(),
+                ErrorDetails = env.IsDevelopment() ? validationException.Failures : new Dictionary<string, string[]>(),
+            });
+        }
+
+        private string ToCamelCase(string input)
+        {
+            return input.Length <= 1 ? input : $"{input.ToLowerInvariant()[0]}{input.Substring(1)}";
         }
     }
 }
