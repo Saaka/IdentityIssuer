46bcc28ac9a91f458185740b99884939650e0b58
Michal Sakwa <saka1988@gmail.com>
2019-12-13 00:20:14 +0100

    Added user constants for validation purposes.

--- IdentityIssuer.Application/Users/Commands/RegisterUserWithCredentials/RegisterUserWithCredentialsCommandValidator.cs
+++ IdentityIssuer.Application/Users/Commands/RegisterUserWithCredentials/RegisterUserWithCredentialsCommandValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using IdentityIssuer.Common.Constants;
 
 namespace IdentityIssuer.Application.Users.Commands.RegisterUserWithCredentials
 {
@@ -7,14 +8,15 @@ namespace IdentityIssuer.Application.Users.Commands.RegisterUserWithCredentials
         public RegisterUserWithCredentialsCommandValidator()
         {
             RuleFor(x => x.DisplayName)
+                .Length(UserConstants.MinDisplayNameLength, UserConstants.MaxDisplayNameLength)
                 .NotEmpty();
-
             RuleFor(x => x.Password)
+                .Length(UserConstants.MinPasswordLength, UserConstants.MaxPasswordLength)
                 .NotEmpty();
-            
             RuleFor(x => x.Email)
                 .NotEmpty()
-                .EmailAddress();
+                .EmailAddress()
+                .Length(UserConstants.MinEmailLength, UserConstants.MaxPasswordLength);
         }
     }
 }
