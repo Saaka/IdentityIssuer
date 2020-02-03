ca6757343dfdff4f8d067c9d668240ef804470fb
Michal Sakwa <saka1988@gmail.com>
2020-02-03 23:49:14 +0100

    Added command validator for AuthorizeUserWithGoogleCommand

---
+++ IdentityIssuer.Application/Users/Commands/AuthorizeUserWithGoogle/AuthorizeUserWithGoogleCommandValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace IdentityIssuer.Application.Users.Commands.AuthorizeUserWithGoogle
+{
+    public class AuthorizeUserWithGoogleCommandValidator : AbstractValidator<AuthorizeUserWithGoogleCommand>
+    {
+        public AuthorizeUserWithGoogleCommandValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty();
+            RuleFor(x => x.Tenant)
+                .NotNull();
+            RuleFor(x => x.Tenant.TenantCode)
+                .NotEmpty();
+        }   
+    }
+}
