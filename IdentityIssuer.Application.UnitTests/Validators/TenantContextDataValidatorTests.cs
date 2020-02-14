c1ce054843ebd30825f33fff5b5454ac4b1fd770
Michal Sakwa <saka1988@gmail.com>
2020-02-14 18:32:18 +0100

    Added unit tests project for Application layer.

---
+++ IdentityIssuer.Application.UnitTests/Validators/TenantContextDataValidatorTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace IdentityIssuer.Application.UnitTests.Validators
+{
+    [Trait("Application", "Validators")]
+    public class TenantContextDataValidatorTests
+    {
+        [Fact]
+        public void Fact()
+        {
+            
+        }
+    }
+}
