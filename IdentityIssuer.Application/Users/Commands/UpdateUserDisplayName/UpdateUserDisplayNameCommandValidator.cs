deb8aaab56a340ba3d3f5c5d4e5e158b0346a253
Michal Sakwa <saka1988@gmail.com>
2020-02-14 18:09:46 +0100

    Added validator for UpdateUserDisplayNameCommand.

--- IdentityIssuer.Application/Users/Commands/UpdateUserDisplayName/UpdateUserDisplayNameCommandValidator.cs
+++ IdentityIssuer.Application/Users/Commands/UpdateUserDisplayName/UpdateUserDisplayNameCommandValidator.cs
@@ -4,6 +4,14 @@ namespace IdentityIssuer.Application.Users.Commands.UpdateUserDisplayName
 {
     public class UpdateUserDisplayNameCommandValidator : AbstractValidator<UpdateUserDisplayNameCommand>
     {
-        
+        public UpdateUserDisplayNameCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+            RuleFor(x => x.UserGuid)
+                .NotEmpty();
+            RuleFor(x => x.User)
+                .IsValid();
+        }
     }
 }
