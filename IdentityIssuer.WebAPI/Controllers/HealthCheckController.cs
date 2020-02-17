f3d2754dfeac54eddbed955b3da9f86200ac3c7e
Michal Sakwa <saka1988@gmail.com>
2020-02-18 00:24:57 +0100

    Replaced placeholder values controller with simple health check controller.

---
+++ IdentityIssuer.WebAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityIssuer.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
