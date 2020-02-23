e6f58cbd7611225080da2d382811d709dd7a298a
Michal Sakwa <saka1988@gmail.com>
2020-02-23 12:57:42 +0100

    Replaced exception messages with simple codes.

--- Src/IdentityIssuer.Common/Constants/Exceptions.cs
+++ Src/IdentityIssuer.Common/Constants/Exceptions.cs
@@ -4,9 +4,18 @@ namespace IdentityIssuer.Common.Constants
     {
         public class UnauthorizedAccessException
         {
-            public const string MissingTenantContextData = "Could not establish Tenant from current context";
-            public const string KidMissmatch = "Current tenant does not match JWT Kid header";
-            public const string MissingTenantTokenSecret = "Could not get current Tenant token secret key";
+            /// <summary>
+            /// Could not establish Tenant from current context
+            /// </summary>
+            public const string MissingTenantContextData = nameof(MissingTenantContextData);
+            /// <summary>
+            /// Current tenant does not match JWT Kid header
+            /// </summary>
+            public const string KidMissmatch = nameof(KidMissmatch);
+            /// <summary>
+            /// Could not get current Tenant token secret key
+            /// </summary>
+            public const string MissingTenantTokenSecret = nameof(MissingTenantTokenSecret);
         }
     }
 }
