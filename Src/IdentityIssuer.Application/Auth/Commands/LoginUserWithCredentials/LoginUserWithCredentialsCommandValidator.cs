b3366c1dea0f8fab005f95ec16368db3f9a82838
Michal Sakwa <saka1988@gmail.com>
2020-04-10 21:28:35 +0200

    Added validator for login command.

--- Src/IdentityIssuer.Application/Auth/Commands/LoginUserWithCredentials/LoginUserWithCredentialsCommandValidator.cs
+++ Src/IdentityIssuer.Application/Auth/Commands/LoginUserWithCredentials/LoginUserWithCredentialsCommandValidator.cs
@@ -1,7 +1,22 @@
+using FluentValidation;
+using IdentityIssuer.Common.Enums;
+
 namespace IdentityIssuer.Application.Auth.Commands.LoginUserWithCredentials
 {
-    public class LoginUserWithCredentialsCommandValidator
+    public class LoginUserWithCredentialsCommandValidator: AbstractValidator<LoginUserWithCredentialsCommand>
     {
-        
+        public LoginUserWithCredentialsCommandValidator()
+        {
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessageCode(ValidationErrorCode.UserPasswordRequired);
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessageCode(ValidationErrorCode.UserEmailRequired)
+                .EmailAddress()
+                .WithMessageCode(ValidationErrorCode.UserEmailInvalid);
+            RuleFor(x => x.Tenant)
+                .IsValid();
+        }
     }
 }
