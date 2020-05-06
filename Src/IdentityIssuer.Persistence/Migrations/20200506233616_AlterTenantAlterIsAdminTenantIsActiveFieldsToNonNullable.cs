25ecb0e40d29966ad549feba49f04b0f707275e6
Michal Sakwa <saka1988@gmail.com>
2020-05-07 01:40:14 +0200

    Added migration to update first tenant to be admin tenant.

---
+++ Src/IdentityIssuer.Persistence/Migrations/20200506233616_AlterTenantAlterIsAdminTenantIsActiveFieldsToNonNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityIssuer.Persistence.Migrations
+{
+    public partial class AlterTenantAlterIsAdminTenantIsActiveFieldsToNonNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            AddAdminTenant(migrationBuilder);
+        }
+
+        private static void AddAdminTenant(MigrationBuilder migrationBuilder)
+        {
+            const string schema = PersistenceConstants.DefaultIdentitySchema;
+            migrationBuilder.Sql($"UPDATE	T " +
+                                 $"SET		T.IsAdminTenant = 1 " +
+                                 $"FROM	    {schema}.Tenants T " +
+                                 $"WHERE	T.Id IN " +
+                                 $"(SELECT	TOP 1 FT.Id " +
+                                 $"FROM	{schema}.Tenants FT " +
+                                 $"ORDER BY FT.Id ASC)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            RemoveAdminTenant(migrationBuilder);
+        }
+
+        private static void RemoveAdminTenant(MigrationBuilder migrationBuilder)
+        {
+            const string schema = PersistenceConstants.DefaultIdentitySchema;
+            migrationBuilder.Sql($"UPDATE	{schema}.Tenants" +
+                                 $"SET		IsAdminTenant = 0");
+        }
+    }
+}
