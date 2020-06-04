889c128f53994b563b761df60e62f3c26081ae74
Michal Sakwa <saka1988@gmail.com>
2020-06-04 22:37:16 +0200

    Added default values for language in tenant settings.

---
+++ Src/IdentityIssuer.Persistence/Migrations/20200604203216_UpdateTenantSettingsWithDefaultLanguage.cs
@@ -0,0 +1,33 @@
+﻿using IdentityIssuer.Common.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityIssuer.Persistence.Migrations
+{
+    public partial class UpdateTenantSettingsWithDefaultLanguage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            AddDefaultLanguages(migrationBuilder);
+        }
+
+        private static void AddDefaultLanguages(MigrationBuilder migrationBuilder)
+        {
+            const string schema = PersistenceConstants.DefaultIdentitySchema;
+            byte defaultLanguage = (byte)LanguageCode.EN;
+            migrationBuilder.Sql($"UPDATE {schema}.TenantSettings " +
+                                 $"SET    DefaultLanguage = {defaultLanguage.ToString()}");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            ResetDefaultLanguages(migrationBuilder);
+        }
+
+        private static void ResetDefaultLanguages(MigrationBuilder migrationBuilder)
+        {
+            const string schema = PersistenceConstants.DefaultIdentitySchema;
+            migrationBuilder.Sql($"UPDATE {schema}.TenantSettings " +
+                                 $"SET    DefaultLanguage = null");
+        }
+    }
+}
